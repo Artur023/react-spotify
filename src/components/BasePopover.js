9e961db3f62973ad8381a5b80ece4634eadbb5ab
Igor Babko <i.i.babko@gmail.com>
2022-09-04 11:15:05 +0300

    75: Reuse useClickAway hook

--- src/components/BasePopover.js
+++ src/components/BasePopover.js
@@ -1,9 +1,4 @@
-import {
-  useState,
-  useRef,
-  useImperativeHandle,
-  forwardRef,
-} from 'react';
+import { useState, useRef, useImperativeHandle, forwardRef } from 'react';
 import useClickAway from '../hooks/useClickAway';
 import usePosition from '../hooks/usePopoverPosition';
 import BaseButton from './BaseButton';

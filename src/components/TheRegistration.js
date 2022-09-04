4c5e9a62b9e23810b27643b521c2fa93cf699033
Igor Babko <i.i.babko@gmail.com>
2022-09-04 11:30:48 +0300

    76: Reuse BaseButton component

--- src/components/TheRegistration.js
+++ src/components/TheRegistration.js
@@ -1,5 +1,5 @@
+import BaseButton from './BaseButton';
 import TheRegistrationInfo from './TheRegistrationInfo';
-import TheRegistrationButton from './TheRegistrationButton';
 
 function TheRegistration() {
   return (
@@ -8,7 +8,7 @@ function TheRegistration() {
       className="bg-gradient-to-r from-[#af2896] to-[#509bf5] text-white py-4 px-8 flex justify-between items-center flex-wrap gap-x-6 gap-y-2"
     >
       <TheRegistrationInfo />
-      <TheRegistrationButton />
+      <BaseButton primary>Sign up free</BaseButton>
     </a>
   );
 }

87df195750c93340a6810323b2658cc3ed1d4223
Igor Babko <i.i.babko@studnote.com>
2022-09-11 23:29:22 +0300

    87 - useEvent in useClickAway

--- src/hooks/useClickAway.js
+++ src/hooks/useClickAway.js
@@ -1,15 +1,11 @@
-import { useEffect } from 'react';
+import useEvent from './useEvent';
 
-function useClickAway(ref, handle, shouldHandle = () => true) {
-  useEffect(() => {
-    function handleMousedown(event) {
-      if (shouldHandle(event) && !ref.current.contains(event.target)) handle();
-    }
+function useClickAway(ref, handler, shouldHandle = () => true) {
+  useEvent('mousedown', handleMousedown);
 
-    document.addEventListener('mousedown', handleMousedown);
-
-    return () => document.removeEventListener('mousedown', handleMousedown);
-  });
+  function handleMousedown(event) {
+    if (shouldHandle(event) && !ref.current.contains(event.target)) handler();
+  }
 }
 
 export default useClickAway;

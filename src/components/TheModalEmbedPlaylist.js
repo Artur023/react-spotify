7106d0f71980074e6621d7ba7897950854dfc912
Igor Babko <i.i.babko@studnote.com>
2022-09-17 13:07:10 +0300

    97: checkbox

--- src/components/TheModalEmbedPlaylist.js
+++ src/components/TheModalEmbedPlaylist.js
@@ -9,9 +9,20 @@ function TheModalEmbedPlaylist({ onClose: handleClose }) {
       </h1>
       <div className="py-6 px-8 text-neutral-500 text-[13px]">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, sint
-        omnis ducimus <a href="/" className="text-white font-bold hover:underline">dolore cumque cum</a>!
+        omnis ducimus{' '}
+        <a href="/" className="text-white font-bold hover:underline">
+          dolore cumque cum
+        </a>
+        !
       </div>
-      <div className="text-right pb-6 px-8">
+      <div className="flex justify-end items-center text-right pb-6 px-8">
+        <label className="inline-flex items-center gap-2 mr-4">
+          <input
+            type="checkbox"
+            className="text-[#1bd760] bg-transparent rounded-sm border-neutral-500 hover:border-[#1bd760] !ring-0 !ring-offset-0 checked:bg-[#1bd760] checked:bg-checkbox"
+          />
+          <span className="text-sm text-neutral-400">Show code</span>
+        </label>
         <BaseButton accent>Copy</BaseButton>
       </div>
     </BaseModal>

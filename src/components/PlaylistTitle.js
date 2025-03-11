b8b50b47c378bd36703166a5f79e9821a7361cb0
Artur023 <artur.023@mail.ru>
2025-03-11 21:36:52 +0400

    feat: components and playlist

---
+++ src/components/PlaylistTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlaylistTitle(props) {
+  return (
+    <h3 className="mt-4 mb-1 font-semibold tracking-wide capitalize">
+      Playlist title
+    </h3>
+  )
+}
+
+export default PlaylistTitle
